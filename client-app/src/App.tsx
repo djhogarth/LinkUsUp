60cad7848229e090eb0e0410d7ca74b81f03846e
DJ Hogarth <dariyhogarth@hotmail.com>
2022-08-01 22:37:51 -0400

    Changed html to use mui (material react) for the header and list of activities

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
-import { AppBar, Icon } from '@mui/material';
+import { AppBar, Box, Icon, IconButton, List, ListItem, Toolbar, Typography} from '@mui/material';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro'
+import { spacing } from '@mui/system';
 
-function App() 
-{
+function App() {
   const [activities, setActivities] = useState([]);
 
   useEffect(() => {
@@ -13,26 +15,28 @@ function App()
       setActivities(response.data);
     })
   }, [])
-  
+
   return (
-    <div className="App">
-      <AppBar position='static'> 
-        <Icon></Icon>
+    <div>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position='static' color='default'>
+          <Toolbar>
+            <IconButton color="inherit" sx={{mr: 1}}>
+              <FontAwesomeIcon icon={solid('users')} size='2x' />
+            </IconButton>            
+            <Typography variant="h3">LinkUsUp</Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
 
+      <List>
+        {activities.map((activity: any) => (
+          <ListItem key={activity.id}>
+            {activity.title}
+          </ListItem>
+        ))}
+      </List>      
 
-      </AppBar>
-      <header className="App-header">
-       
-        <ul>
-          {activities.map((activity: any) => (
-            <li key={activity.id}>
-              {activity.title}
-            </li>            
-          ))}
-        </ul>
-
-       
-      </header>
     </div>
   );
 }
