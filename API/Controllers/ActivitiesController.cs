c5e87804be50e78fca2840bd7dfb17900e691e19
DJ Hogarth <dariyhogarth@hotmail.com>
2022-09-04 20:06:17 -0400

    refactored the activity controller and base api controller to make use of the new query and command handlers

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,31 +1,38 @@
 
 using Domain;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
+using MediatR;
+using Application.Activities;
 
 namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly DatabaseContext _databaseContext;
-        public ActivitiesController(DatabaseContext databaseContext)
+        private readonly IMediator _mediator;
+        
+        public ActivitiesController(IMediator mediator)
         {
-            _databaseContext = databaseContext;
+            _mediator = mediator;            
         }
 
         [HttpGet]
 
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await _databaseContext.Activities.ToListAsync();
+            return await _mediator.Send(new ActivityList.Query());
         }
 
         [HttpGet("{id}")]
 
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return await _databaseContext.Activities.FindAsync(id);
+            return await Mediator.Send(new FindActivity.Query{Id = id});
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity (Activity activity)
+        {
+            return Ok(await Mediator.Send(new Create.Command {Activity = activity}));
         }
 
     }
